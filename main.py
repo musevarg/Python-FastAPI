9dabd38e051c791902c8bc0d8aecdaafbe51cf0f
musevarg <gravesum5@gmail.com>
2024-04-01 21:14:47 +0200

    get quote in json from db

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from typing import Union
 from fastapi import FastAPI
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from pydantic import BaseModel
+from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 
+Base = declarative_base()
 engine = create_engine('postgresql+psycopg2://max2:pswd@localhost/pgdb')
+Base.metadata.create_all(engine)
 
+class QuoteObject(Base):
+    __tablename__ = "Quotes"
+    id = Column(Integer, primary_key=True)
+    quote = Column(String)
+    author = Column(String)
 
-class QuoteObject(BaseModel):
-    Id: int
-    Quote: str
-    Author: str
-
+PydanticQuote = sqlalchemy_to_pydantic(QuoteObject)
 
 def getQuote(id):
     with engine.connect() as conn:
         result = conn.execution_options(stream_results=True).execute(text("SELECT Id, Quote, Author fROM Quotes WHERE Id = " + str(id)))
-        parsed_result = result.fetchone()
-        return parsed_result
+        row = result.fetchone()
+        return row
 
 
 app = FastAPI()
@@ -28,4 +33,6 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    quote = PydanticQuote.from_orm(getQuote(item_id))
+    print(quote)
+    return quote
