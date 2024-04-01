4cff785371446575cd9c48baa5b399eadd190c85
musevarg <gravesum5@gmail.com>
2024-04-01 20:26:55 +0200

    connect to db

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from typing import Union
-
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+
+engine = create_engine('postgresql+psycopg2://max2:pswd@localhost/pgdb')
+
+def getQuote(id):
+    with engine.connect() as conn:
+        result = conn.execution_options(stream_results=True).execute(text("SELECT Id, Quote, Author fROM Quotes WHERE Id = " + str(id)))
+        parsed_result = result.fetchone()
+        return parsed_result
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
