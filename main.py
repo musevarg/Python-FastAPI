a557f604d785f8cab59ee6ec76b92b94bb375c8a
musevarg <gravesum5@gmail.com>
2024-04-02 16:44:53 +0200

    post method

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from typing import Union
-from fastapi import FastAPI
-from sqlalchemy import create_engine, text, insert, Column, Integer, String
+from fastapi import FastAPI, Request
+from sqlalchemy import create_engine, text, insert, Column, Integer, String, Table, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from pydantic import BaseModel
 from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 
 Base = declarative_base()
-engine = create_engine('postgresql+psycopg2://max2:pswd@localhost/pgdb')
+engine = create_engine('postgresql+psycopg2://max2:pswd@localhost/pgdb',future=True)
 Base.metadata.create_all(engine)
+
+metadata = MetaData()
+quote_table = Table('Quotes',
+                    metadata,
+                    Column('Id', Integer, primary_key=True),
+                    Column('Quote', String),
+                    Column('Author', String)
+                    )
 
 class QuoteObject(Base):
     __tablename__ = "Quotes"
@@ -19,10 +27,17 @@ PydanticQuote = sqlalchemy_to_pydantic(QuoteObject)
 
 def getQuote(id):
     with engine.connect() as conn:
-        result = conn.execution_options(stream_results=True).execute(text("SELECT Id, Quote, Author fROM Quotes WHERE Id = " + str(id)))
+        result = conn.execution_options(stream_results=True).execute(text("SELECT Id, Quote, Author FROM Quotes WHERE Id = " + str(id)))
         row = result.fetchone()
         return row
 
+def insertQuote(q: QuoteObject):
+    stmt = insert(quote_table).values(Id=q.id, Quote=q.quote, Author=q.author)
+    print(stmt)
+    with engine.connect() as conn:
+        result = conn.execute(stmt)
+        conn.commit()
+        print('Inserted Primary Key: %s' % str(result.inserted_primary_key))
 
 app = FastAPI()
 
@@ -40,5 +55,9 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 
 @app.post("/items/")
-async def create_item(quote: QuoteObject):
+async def create_item(request: Request):
+    req = await request.json()
+    quote = QuoteObject(**req)
+    print(quote.author)
+    insertQuote(quote)
     return quote
