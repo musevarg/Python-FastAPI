83715285c41127168540883bee5514ce4d774a9a
musevarg <gravesum5@gmail.com>
2024-05-16 13:22:13 +0200

    adding unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -1,20 +1,31 @@
 import unittest
+import main
+from main import app
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_root(self):
+        root_value = main.read_root()
+        self.assertEqual(root_value['Hello'], 'World')
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_get_quote(self):
+        first_quote = main.read_item(1)
+        self.assertEqual(first_quote.id, 1)
+        self.assertEqual(first_quote.author, 'Lemmy Kilmister')
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_insert_quote(self):
+        response = client.post(
+        "/items/",
+        headers={"X-Token": "coneofsilence"},
+        json={"id": 100, "quote":"New quote", "author":"New author"},
+        )
+        assert response.status_code == 200
+        assert response.json() == {"id": 100, "quote":"New quote", "author":"New author"}
+
 
 if __name__ == '__main__':
     unittest.main()
